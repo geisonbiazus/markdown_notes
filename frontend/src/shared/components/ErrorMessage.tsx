b96231d03d4f275dc41c65981fcb2c33027f3b07
Geison Biazus <geisonbiazus@gmail.com>
2020-08-11 09:22:52 +0200

    Add i18n to error messages

--- frontend/src/shared/components/ErrorMessage.tsx
+++ frontend/src/shared/components/ErrorMessage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export interface ErrorMessageProps {
   field: string;
@@ -6,11 +7,9 @@ export interface ErrorMessageProps {
 }
 
 export const ErrorMessage: React.FC<ErrorMessageProps> = ({ field, type }) => {
+  const { t } = useTranslation();
+
   if (!type) return null;
 
-  return (
-    <div>
-      {field} {type}
-    </div>
-  );
+  return <div>{t(`validation.${type}`, { field: t(field) })}</div>;
 };
