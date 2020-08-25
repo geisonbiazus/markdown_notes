5d50e6502501bd0cac41ed76e12c5fafd6cf108a
Geison Biazus <geisonbiazus@gmail.com>
2020-08-25 20:59:19 +0200

    Add some styles to NoteList

--- frontend/src/notes/ui/NoteList.tsx
+++ frontend/src/notes/ui/NoteList.tsx
@@ -2,25 +2,34 @@ import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { NewNoteButton } from './NewNoteButton';
+import './NoteList.css';
 
 export const NoteList: React.FC = () => {
   return (
-    <>
+    <div className="note-list-container">
       <NewNoteButton />
-      <Nav defaultActiveKey="/home" className="flex-column">
-        <Nav.Item>
-          <Link to="/notes/123">Note 1</Link>
+      <Nav defaultActiveKey="/home" className="flex-column" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link active={true} as={Link} to="/notes/123">
+            Note 1
+          </Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Link to="/notes/123">Note 1</Link>
+        <Nav.Item as="li">
+          <Nav.Link as={Link} to="/notes/123">
+            Note 1
+          </Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Link to="/notes/123">Note 1</Link>
+        <Nav.Item as="li">
+          <Nav.Link as={Link} to="/notes/123">
+            Note 1
+          </Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Link to="/notes/123">Note 1</Link>
+        <Nav.Item as="li">
+          <Nav.Link as={Link} to="/notes/123">
+            Note 1
+          </Nav.Link>
         </Nav.Item>
       </Nav>
-    </>
+    </div>
   );
 };
