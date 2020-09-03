b54600a82e0a8d9182f28d841e0101704878f8a4
Geison Biazus <geisonbiazus@gmail.com>
2020-09-03 09:02:01 +0200

    Add confirmation modal example

--- frontend/src/notes/ui/NoteList.tsx
+++ frontend/src/notes/ui/NoteList.tsx
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { NewNoteButton } from './NewNoteButton';
 import { useNoteContext } from './NoteContext';
 import { VerticalNav, NavItem, NavIcon } from '../../shared/components';
+import { Modal, Button } from 'react-bootstrap';
 
 export const NoteList: React.FC = () => {
-  const { listNoteState, editNoteState, getNotes } = useNoteContext();
+  const { listNoteState, editNoteState, getNotes, requestNoteRemoval } = useNoteContext();
 
   useEffect(() => {
     getNotes();
@@ -12,6 +13,7 @@ export const NoteList: React.FC = () => {
 
   return (
     <>
+      <RemoveNoteConfirmModal />
       <NewNoteButton />
       <VerticalNav>
         {listNoteState.notes.map((note) => (
@@ -21,10 +23,33 @@ export const NoteList: React.FC = () => {
             href={`/notes/${note.id}`}
             active={note.id === editNoteState.note.id}
           >
-            <NavIcon />
+            <NavIcon onClick={() => requestNoteRemoval(note)} />
           </NavItem>
         ))}
       </VerticalNav>
     </>
+  );
+};
+
+export interface RemoveNoteConfirmModalProps {}
+
+export const RemoveNoteConfirmModal: React.FC = () => {
+  const handleClose = () => {};
+
+  return (
+    <Modal show={true} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Remove note</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to remove this note?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={handleClose}>
+          Remove Note
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
