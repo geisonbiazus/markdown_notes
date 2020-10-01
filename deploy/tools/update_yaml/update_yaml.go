d88cb20fff10a20fb67f00bd960e74af12a46ceb
Geison Biazus <geisonbiazus@gmail.com>
2020-10-01 09:27:21 +0200

    Update key hierarchies

--- deploy/tools/update_yaml/update_yaml.go
+++ deploy/tools/update_yaml/update_yaml.go
@@ -5,34 +5,63 @@ import (
 )
 
 func UpdateYAML(yamlString string, hierarchy ...string) (string, error) {
-	key := hierarchy[0]
-
-	if key == "" {
-		return yamlString, nil
-	}
-
 	parsedYAML := map[interface{}]interface{}{}
 
 	yaml.Unmarshal([]byte(yamlString), &parsedYAML)
 
-	putIn(parsedYAML, hierarchy...)
+	parsedYAML = putIn(parsedYAML, hierarchy...)
 
 	updatedYAML, _ := yaml.Marshal(&parsedYAML)
 
 	return string(updatedYAML), nil
 }
 
-func putIn(parsedYAML map[interface{}]interface{}, hierarchy ...string) map[interface{}]interface{} {
-	// currentNode := parsedYAML
+// func putIn(parsedYAML map[interface{}]interface{}, hierarchy ...string) map[interface{}]interface{} {
 
-	// for _, key := range(hierarchy) {
+// 	if len(hierarchy) == 2 {
+// 		key := hierarchy[0]
+// 		value := hierarchy[1]
 
-	// }
+// 		parsedYAML[key] = value
+// 	} else if len(hierarchy) == 3 {
+// 		key1 := hierarchy[0]
+// 		key2 := hierarchy[1]
+// 		value := hierarchy[2]
+
+// 		if _, ok := parsedYAML[key1]; !ok {
+// 			parsedYAML[key1] = map[interface{}]interface{}{}
+// 		}
+
+// 		if _, ok := parsedYAML[key1].(string); ok {
+// 			parsedYAML[key1] = map[interface{}]interface{}{}
+// 		}
+
+// 		if node, ok := parsedYAML[key1].(map[interface{}]interface{}); ok {
+// 			node[key2] = value
+// 		}
+// 	}
+
+// 	return parsedYAML
+// }
+func putIn(node map[interface{}]interface{}, hierarchy ...string) map[interface{}]interface{} {
+	if len(hierarchy) < 2 {
+		return node
+	}
 
 	key := hierarchy[0]
 	value := hierarchy[1]
 
-	parsedYAML[key] = value
+	if len(hierarchy) == 2 {
+		node[key] = value
+	} else {
+		nextNode := map[interface{}]interface{}{}
 
-	return parsedYAML
+		if current, ok := node[key].(map[interface{}]interface{}); ok {
+			nextNode = current
+		}
+
+		node[key] = putIn(nextNode, hierarchy[1:]...)
+	}
+
+	return node
 }
--- deploy/tools/update_yaml/update_yaml_test.go
+++ deploy/tools/update_yaml/update_yaml_test.go
@@ -6,29 +6,30 @@ import (
 )
 
 func TestUpdateYAML(t *testing.T) {
-	t.Run("Given an empty YAML and an empty key, it returns the empty YAML", func(t *testing.T) {
+	t.Run("Given an empty YAML and no value, it returns the empty YAML", func(t *testing.T) {
 		yamlString := ""
-		key := ""
-		value := ""
-		updatedYAML, _ := UpdateYAML(yamlString, key, value)
+		updatedYAML, _ := UpdateYAML(yamlString)
 
-		assertEqual(t, "", updatedYAML)
+		assertEqual(t, "{}\n", updatedYAML)
+	})
+
+	t.Run("Given an empty YAML and a key and no value, it returns the empty YAML", func(t *testing.T) {
+		yamlString := ""
+		updatedYAML, _ := UpdateYAML(yamlString, "key")
+
+		assertEqual(t, "{}\n", updatedYAML)
 	})
 
 	t.Run("Given an empty YAML and a key and a value, it returns the YAML with that key and value", func(t *testing.T) {
 		yamlString := ""
-		key := "key"
-		value := "value"
-		updatedYAML, _ := UpdateYAML(yamlString, key, value)
+		updatedYAML, _ := UpdateYAML(yamlString, "key", "value")
 
 		assertEqual(t, "key: value\n", updatedYAML)
 	})
 
 	t.Run("Given an YAML with values and a key and a value, it returns the YAML with that key and value apended", func(t *testing.T) {
 		yamlString := "key1: value1\n"
-		key := "key2"
-		value := "value2"
-		updatedYAML, _ := UpdateYAML(yamlString, key, value)
+		updatedYAML, _ := UpdateYAML(yamlString, "key2", "value2")
 
 		assertEqual(t, ""+
 			"key1: value1\n"+
@@ -40,9 +41,7 @@ func TestUpdateYAML(t *testing.T) {
 		yamlString := "" +
 			"key1: value1\n" +
 			"key2: value2\n"
-		key := "key1"
-		value := "value3"
-		updatedYAML, _ := UpdateYAML(yamlString, key, value)
+		updatedYAML, _ := UpdateYAML(yamlString, "key1", "value3")
 
 		assertEqual(t, ""+
 			"key1: value3\n"+
@@ -50,22 +49,152 @@ func TestUpdateYAML(t *testing.T) {
 			updatedYAML)
 	})
 
-	// t.Run("Given a more arguments, it puts the value in the hierarchy", func(t *testing.T) {
-	// 	yamlString := ""
-	// 	parent := "parent"
-	// 	child := "child"
-	// 	value := "value"
-	// 	updatedYAML, _ := UpdateYAML(yamlString, parent, child, value)
+	t.Run("Given more arguments, it puts the value in the hierarchy", func(t *testing.T) {
+		yamlString := ""
+		updatedYAML, _ := UpdateYAML(yamlString, "parent", "child", "value")
+
+		assertEqual(t, ""+
+			"parent:\n"+
+			"  child: value\n",
+			updatedYAML)
+	})
+
+	t.Run("Given and existent YAML and more than 3 arguments, it puts the value in the hierarchy keeping the previous values", func(t *testing.T) {
+		yamlString := "" +
+			"key1: value1\n" +
+			"parent:\n" +
+			"  child1: value1\n"
+
+		updatedYAML, _ := UpdateYAML(yamlString, "parent", "child2", "value2")
+
+		assertEqual(t, ""+
+			"key1: value1\n"+
+			"parent:\n"+
+			"  child1: value1\n"+
+			"  child2: value2\n",
+			updatedYAML)
+	})
+
+	t.Run("Given a structure that colides with a value in the middle of the hierarchy, it overrides", func(t *testing.T) {
+		yamlString := "" +
+			"key1: value1\n" +
+			"parent: child\n"
+
+		updatedYAML, _ := UpdateYAML(yamlString, "parent", "child", "value")
+
+		assertEqual(t, ""+
+			"key1: value1\n"+
+			"parent:\n"+
+			"  child: value\n",
+			updatedYAML)
+	})
+
+	t.Run("Given a structure with a hierarchy and a value that collides in the middle of the hierarchy, it overrides", func(t *testing.T) {
+		yamlString := "" +
+			"key1: value1\n" +
+			"parent:\n" +
+			"  child: value\n"
+
+		updatedYAML, _ := UpdateYAML(yamlString, "parent", "value")
+
+		assertEqual(t, ""+
+			"key1: value1\n"+
+			"parent: value\n",
+			updatedYAML)
+	})
+
+	t.Run("works with structures of more than 3 levels", func(t *testing.T) {
+		yamlString := ""
+
+		updatedYAML, _ := UpdateYAML(yamlString, "key1", "key2", "key3", "value")
+
+		assertEqual(t, ""+
+			"key1:\n"+
+			"  key2:\n"+
+			"    key3: value\n",
+			updatedYAML)
+	})
+
+	// t.Run("Acceptance tests", func(t *testing.T) {
+	// 	yamlString := "" +
+	// 		"version: '3.8'\n" +
+	// 		"services:\n" +
+	// 		"  backend:\n" +
+	// 		"    image: geisonbiazus/markdown_notes_backend:alpha\n" +
+	// 		"    restart: always\n" +
+	// 		"    env_file: .env\n" +
+	// 		"    environment:\n" +
+	// 		"      DB_HOST: postgres\n" +
+	// 		"      VIRTUAL_HOST: api.notes.geisonbiazus.com\n" +
+	// 		"      LETSENCRYPT_HOST: api.notes.geisonbiazus.com\n" +
+	// 		"      DB_USERNAME: main\n" +
+	// 		"      DB_NAME: markdown_notes\n" +
+	// 		"    ports:\n" +
+	// 		"      - 4000:4000\n" +
+	// 		"  frontend:\n" +
+	// 		"    image: geisonbiazus/markdown_notes_frontend:alpha\n" +
+	// 		"    restart: always\n" +
+	// 		"    environment:\n" +
+	// 		"      REACT_APP_API_URL: https://api.notes.geisonbiazus.com\n" +
+	// 		"      REACT_APP_APP_ENV: production\n" +
+	// 		"      VIRTUAL_HOST: notes.geisonbiazus.com\n" +
+	// 		"      LETSENCRYPT_HOST: notes.geisonbiazus.com\n" +
+	// 		"    ports:\n" +
+	// 		"      - 3000:3000\n" +
+	// 		"  postgres:\n" +
+	// 		"    image: postgres:12\n" +
+	// 		"    restart: always\n" +
+	// 		"    env_file: .env\n" +
+	// 		"    environment:\n" +
+	// 		"      POSTGRES_DB: markdown_notes\n" +
+	// 		"    ports:\n" +
+	// 		"      - 5432:5432\n" +
+	// 		"    volumes:\n" +
+	// 		"      - ./volumes/postgres-data:/var/lib/postgresql/data\n"
+
+	// 	updatedYAML, _ := UpdateYAML(yamlString, "services", "backend", "image", "geisonbiazus/markdown_notes_backend:1234")
 
 	// 	assertEqual(t, ""+
-	// 		"parent:\n"+
-	// 		"  child: value\n",
+	// 		"version: '3.8'\n"+
+	// 		"services:\n"+
+	// 		"  backend:\n"+
+	// 		"    image: geisonbiazus/markdown_notes_backend:1234\n"+
+	// 		"    restart: always\n"+
+	// 		"    env_file: .env\n"+
+	// 		"    environment:\n"+
+	// 		"      DB_HOST: postgres\n"+
+	// 		"      VIRTUAL_HOST: api.notes.geisonbiazus.com\n"+
+	// 		"      LETSENCRYPT_HOST: api.notes.geisonbiazus.com\n"+
+	// 		"      DB_USERNAME: main\n"+
+	// 		"      DB_NAME: markdown_notes\n"+
+	// 		"    ports:\n"+
+	// 		"      - 4000:4000\n"+
+	// 		"  frontend:\n"+
+	// 		"    image: geisonbiazus/markdown_notes_frontend:alpha\n"+
+	// 		"    restart: always\n"+
+	// 		"    environment:\n"+
+	// 		"      REACT_APP_API_URL: https://api.notes.geisonbiazus.com\n"+
+	// 		"      REACT_APP_APP_ENV: production\n"+
+	// 		"      VIRTUAL_HOST: notes.geisonbiazus.com\n"+
+	// 		"      LETSENCRYPT_HOST: notes.geisonbiazus.com\n"+
+	// 		"    ports:\n"+
+	// 		"      - 3000:3000\n"+
+	// 		"  postgres:\n"+
+	// 		"    image: postgres:12\n"+
+	// 		"    restart: always\n"+
+	// 		"    env_file: .env\n"+
+	// 		"    environment:\n"+
+	// 		"      POSTGRES_DB: markdown_notes\n"+
+	// 		"    ports:\n"+
+	// 		"      - 5432:5432\n"+
+	// 		"    volumes:\n"+
+	// 		"      - ./volumes/postgres-data:/var/lib/postgresql/data\n",
 	// 		updatedYAML)
 	// })
 }
 
 func assertEqual(t *testing.T, expected, actual interface{}) {
 	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("Values are not equal.\nExpected: %v\n  Actual: %v", expected, actual)
+		t.Errorf("Values are not equal.\nExpected:\n%v\n\nActual:\n%v", expected, actual)
 	}
 }
