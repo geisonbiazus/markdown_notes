a08b12d0005266097ccc074d5e0ec254b043998a
Geison Biazus <geisonbiazus@gmail.com>
2020-12-10 09:21:25 +0100

    [Backend] Refactor /notes API to JSON Server pattern

--- backend/src/api/controllers/NoteController.ts
+++ backend/src/api/controllers/NoteController.ts
@@ -1,5 +1,6 @@
+import bind from 'bind-decorator';
 import { Request, Response } from 'express';
-import { NoteInteractor, SaveNoteRequest, Note } from '../../notes';
+import { Note, NoteInteractor, SaveNoteRequest } from '../../notes';
 import { InteractorResponse } from '../../utils/interactor';
 import { resolveHttpStatus } from '../helpers';
 
@@ -10,11 +11,12 @@ export class NoteController {
     this.noteInteractor = noteInteractor;
   }
 
-  saveNote = async (req: Request, res: Response): Promise<void> => {
+  @bind
+  public async saveNote(req: Request, res: Response): Promise<void> {
     const request = this.buildSaveNoteRequest(req);
     const response = await this.noteInteractor.saveNote(request);
     this.sendSaveNoteResponse(res, response);
-  };
+  }
 
   private buildSaveNoteRequest(req: Request): SaveNoteRequest {
     return {
@@ -27,31 +29,48 @@ export class NoteController {
   private sendSaveNoteResponse(res: Response, response: InteractorResponse<Note>): void {
     const { status, validationErrors, data } = response;
 
-    res.status(resolveHttpStatus(response));
-    res.json({ status, errors: validationErrors, note: data });
+    if (status === 'success') {
+      res.status(200);
+      res.json(data);
+    } else {
+      res.status(422);
+      res.json(validationErrors);
+    }
   }
 
-  getNote = async (req: Request, res: Response): Promise<void> => {
+  @bind
+  public async getNote(req: Request, res: Response): Promise<void> {
     const response = await this.noteInteractor.getNote(req.params.id);
     const { status, type, data } = response;
 
-    res.status(resolveHttpStatus(response));
-    res.json({ status, type, note: data });
-  };
+    if (status == 'success') {
+      res.status(200);
+      res.json(data);
+    } else {
+      res.status(404);
+      res.json({ type });
+    }
+  }
 
-  getNotes = async (_req: Request, res: Response): Promise<void> => {
+  @bind
+  public async getNotes(_req: Request, res: Response): Promise<void> {
     const response = await this.noteInteractor.getNotes();
-    const { status, data } = response;
 
-    res.status(resolveHttpStatus(response));
-    res.json({ status, notes: data });
-  };
+    res.status(200);
+    res.json(response.data);
+  }
 
-  removeNote = async (req: Request, res: Response): Promise<void> => {
+  @bind
+  public async removeNote(req: Request, res: Response): Promise<void> {
     const response = await this.noteInteractor.removeNote(req.params.id);
     const { status, type } = response;
 
-    res.status(resolveHttpStatus(response));
-    res.json({ status, type });
-  };
+    if (status == 'success') {
+      res.status(200);
+      res.json();
+    } else {
+      res.status(404);
+      res.json({ type });
+    }
+  }
 }
