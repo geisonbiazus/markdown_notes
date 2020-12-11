b6c99c7f54533a7a6b2a6f84a7833d557ba1cfd7
Geison Biazus <geisonbiazus@gmail.com>
2020-12-11 09:13:32 +0100

    Update EditNoteInteractor.ts

--- frontend/src/notes/interactors/EditNoteInteractor.ts
+++ frontend/src/notes/interactors/EditNoteInteractor.ts
@@ -7,7 +7,7 @@ import {
   StateObservableInteractor,
   validateRequired,
 } from '../../utils';
-import { Note, NoteClient, SaveNoteResponse, ValidationErrorResponse } from '../entities';
+import { Note, NoteClient, ValidationErrorResponse } from '../entities';
 import { NoteSavedPayload, NOTE_SAVED_EVENT } from '../events';
 
 export interface EditNoteState {
